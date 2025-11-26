92bca83daa6dda6a68b5cff512bec94a31fd10bf
Sujith <sujithj7123@gmail.com>
2025-11-26 10:47:13 +0530

    send_message func

---
+++ software.py
@@ -0,0 +1,21 @@
+import pywhatkit
+
+def send_message():
+
+    num=input("Enter reciever number: ")
+    print()
+
+    message=input("Enter message: ")
+    print()
+
+    time=input("Time of delievery (in format 07,40): ")
+    
+    split_time=time.split(',')
+    hour=int(split_time[0])
+    min=int(split_time[1])
+
+    pywhatkit.sendwhatmsg(num,message,hour,min)
+
+    
+
+
